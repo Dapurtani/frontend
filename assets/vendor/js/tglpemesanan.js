c99099f0e0954fda9e6acb7a39564615ddca8ac9
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-08-29 12:15:23 +0800

    first commit

---
+++ assets/vendor/js/tglpemesanan.js
@@ -0,0 +1,17 @@
+// untuk nama hari dan bulan
+var hari = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+var bulan = ['January', 'February', 'March', 'April', 'Mey', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// untuk tanggal
+var getwaktu = new Date();
+var _hari = getwaktu.getDay();
+var gettanggal = getwaktu.getDate();
+var _bulan = getwaktu.getMonth();
+var gettahun = getwaktu.getFullYear();
+
+// untuk mengambil bulan wilayah indonesia
+var gethari = hari[_hari];
+var getbulan = bulan[_bulan];
+
+// mengambil waktu pembayaran sekarang
+document.getElementById('getwaktupembayaran').innerHTML = gethari + ", " + gettanggal + " " + getbulan + " " + gettahun;
